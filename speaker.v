cd4c0c584a032f942d9cfbf0220ad75e1da73378
angelamz <31943882+angelamz@users.noreply.github.com>
2018-03-15 19:58:49 -0400

    Add files via upload

---
+++ speaker.v
@@ -0,0 +1,24 @@
+module play(clk, sound, speaker);
+	/* REFERNCED CODE */
+	input sound, clk;
+	output speaker;
+	reg [31:0] clkdivider = (50000000/880);
+
+
+	reg [31:0] counter;
+	always @(posedge clk)
+		begin
+			if(counter==0)
+				counter <= clkdivider-1;
+			else
+				counter <= counter-1;
+		end
+
+	reg speaker;
+	always @(posedge clk)
+		if((counter==0) && sound)
+			speaker <= ~speaker;
+	/* REFERNCE CODE END */
+	
+endmodule
+
