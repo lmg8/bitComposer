18a54789e71a8b4c01658cf4ab709ed3132836e9
angelamz <31943882+angelamz@users.noreply.github.com>
2018-03-15 20:54:36 -0400

    Update shifter.v

--- shifter.v
+++ shifter.v
@@ -1,113 +1,111 @@
-module shift(clk, select, clear, load, out);
-	input clk, clear, load;
+module shift(clock, select, clear, load_b, out);
+	input clock, clear, load_b;
 	input [2:0] select;
 	output out;
 
-	wire [27:0] q;
-	wire rateClk;
+	wire [27:0] Q;
+	wire clk;
 	
-	wire [11:0] pattern;
-	wire [11:0] shiftOut;
+	wire [11:0] morse_code;
 	
-	assign rateClk = (q == 0 ? 1 : 0);
+	LUT lut(select, morse_code);
+	RteDivider rd0(clock, clear, 28'b0001011111010111100000111111, Q);
 	
-	LUT lut(select, pattern);
-	RateDivider rd(clk, clear, 28'b0001011111010111100000111111, q);
-	
-	shifter my_shifter(pattern, clear, load, rateClk, 1'b0, 1'b1, shiftOut);
-	
-	assign out = shiftOut[0];
+	assign clk = (Q == 0 ? 1 : 0);
+	ShiftRegister r0(morse_code, clear, load_b, clk, 1'b0, 1'b1, out);
+
 endmodule
 
-module LUT(select, pattern);
-	input [2:0] select;
-	output reg [11:0] pattern;
 
-	always @(select)
+module LUT(letter, morse_code);
+	input [2:0] letter;
+	output reg [11:0] morse_code;
+
+	always @(letter)
 	begin
-	case(select)
-		3'b000: pattern = 12'b000000011101;
-		3'b001: pattern = 12'b000101010111;
-		3'b010: pattern = 12'b010111010111;
-		3'b011: pattern = 12'b000001010111;
-		3'b100: pattern = 12'b000000000001;
-		3'b101: pattern = 12'b000101110101;
-		3'b110: pattern = 12'b000101110111;
-		3'b111: pattern = 12'b000001010101;
+	case(letter)
+		3'b000: morse_code = 12'b000000011101;
+		3'b001: morse_code = 12'b000101010111;
+		3'b010: morse_code = 12'b010111010111;
+		3'b011: morse_code = 12'b000001010111;
+		3'b100: morse_code = 12'b000000000001;
+		3'b101: morse_code = 12'b000101110101;
+		3'b110: morse_code = 12'b000101110111;
+		3'b111: morse_code = 12'b000001010101;
 	endcase
 	end
 endmodule
 
-module RateDivider(clock, reset, d, q);
-	input clock, reset;
-	input [27:0] d;
-	output reg [27:0] q;
+module RteDivider(clk, reset, timer, Q);
+	input clk, reset;
+	input [27:0] timer;
+	output reg [27:0] Q;
 
-	always @(posedge clock)
+	always @(posedge clk)
 	begin
 		if(reset == 1'b0) 
-			q <= 0;
-		else if(q == 0)
-			q <= d;
+			Q <= 0;
+		else if(Q == 0)
+			Q <= timer;
 		else 
-			q <= q - 1'b1;
+			Q <= Q - 1'b1;
 	end
 endmodule
 
-module shifter(load_val, reset, load, clk, asr, shift, q);
-	input [11:0] load_val;
-	input reset, asr, shift;
+module ShiftRegister(Load_Val, reset, load, clk, in, shift, Q);
+	input [11:0] Load_Val;
+	input reset, in, shift;
 	input load;
 	input clk;
-	output [11:0] q;
+	output Q;
 
-	wire [11:0] shiftWire;
+	wire [11:0] bit;
+	assign Q = bit[0];
 	
-	shifterBit bit11(asr, load_val[11], shift, load, reset, clk, shiftWire[11]);
-	shifterBit bit10(shiftWire[11], load_val[10], shift, load, reset, clk, shiftWire[10]);
-	shifterBit bit9(shiftWire[10], load_val[9], shift, load, reset, clk, shiftWire[9]);
-	shifterBit bit8(shiftWire[9], load_val[8], shift, load, reset, clk, shiftWire[8]);
-	shifterBit bit7(shiftWire[8], load_val[7], shift, load, reset, clk, shiftWire[7]);
-	shifterBit bit6(shiftWire[7], load_val[6], shift, load, reset, clk, shiftWire[6]);
-	shifterBit bit5(shiftWire[6], load_val[5], shift, load, reset, clk, shiftWire[5]);
-	shifterBit bit4(shiftWire[5], load_val[4], shift, load, reset, clk, shiftWire[4]);
-	shifterBit bit3(shiftWire[4], load_val[3], shift, load, reset, clk, shiftWire[3]);
-	shifterBit bit2(shiftWire[3], load_val[2], shift, load, reset, clk, shiftWire[2]);
-	shifterBit bit1(shiftWire[2], load_val[1], shift, load, reset, clk, shiftWire[1]);
-	shifterBit bit0(shiftWire[1], load_val[0], shift, load, reset, clk, shiftWire[0]);
+	ShifterBit sb11(in, Load_Val[11], shift, load, reset, clk, bit[11]);
+	ShifterBit sb10(bit[11], Load_Val[10], shift, load, reset, clk, bit[10]);
+	ShifterBit sb9(bit[10], Load_Val[9], shift, load, reset, clk, bit[9]);
+	ShifterBit sb8(bit[9], Load_Val[8], shift, load, reset, clk, bit[8]);
+	ShifterBit sb7(bit[8], Load_Val[7], shift, load, reset, clk, bit[7]);
+	ShifterBit sb6(bit[7], Load_Val[6], shift, load, reset, clk, bit[6]);
+	ShifterBit sb5(bit[6], Load_Val[5], shift, load, reset, clk, bit[5]);
+	ShifterBit sb4(bit[5], Load_Val[4], shift, load, reset, clk, bit[4]);
+	ShifterBit sb3(bit[4], Load_Val[3], shift, load, reset, clk, bit[3]);
+	ShifterBit sb2(bit[3], Load_Val[2], shift, load, reset, clk, bit[2]);
+	ShifterBit sb1(bit[2], Load_Val[1], shift, load, reset, clk, bit[1]);
+	ShifterBit sb0(bit[1], Load_Val[0], shift, load, reset, clk, bit[0]);
 
-	assign q = shiftWire;
 endmodule
 
-module shifterBit (in, load_val, shift, load_n, reset_n, clk, out);
+module ShifterBit (in, load_val, shift, load_n, reset_n, clk, out);
 	input in, load_val, shift, load_n, reset_n, clk;
 	output out;
 	
-	wire mux_out, mux2_out;
+	wire mux_out;
 	mux2to1 mux1(out, in, shift, mux_out);
 	mux2to1 mux2(load_val, mux_out, load_n, mux2_out);
-	flipflop F0(reset_n, clk, mux2_out, out);
+	DFlipFlop dff(reset_n, clk, mux2_out, out);
 endmodule
 
-module flipflop(reset, clk, d, q);
+module DFlipFlop(reset, clk, d, Q);
 	input clk, reset, d;
-	output reg q;
+	output reg Q;
 
 	always @(posedge clk)
 	begin 
 		if (reset == 1'b0)
-			q <= 1'b0;
+			Q <= 1'b0;
 		else
-			q <= d;
+			Q <= d;
 	end
 endmodule
 
 module mux2to1(x, y, s, m);
-    input x, y, s; 
+    input x;
+    input y; 
+    input s; 
     output m; 
   
     assign m = s & y | ~s & x;
-	 // OR
-    // assign m = s ? y : x;
 
 endmodule
